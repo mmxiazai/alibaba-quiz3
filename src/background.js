d7932f87c90a86d16fb15246bb732e7555c5c3d4
mmxiazai <mmxiazai@qq.com>
2013-08-15 22:47:55 +0800

    Create background.js

---
+++ src/background.js
@@ -0,0 +1,14 @@
+/*chrome.tabs.create({
+  url: 'http://ued.campus.alibaba.com/quiz3/index.php'
+}, function(){});*/
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-18443083-16']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
